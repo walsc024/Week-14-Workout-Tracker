925e190ede4eab2801cbdde7677b13b96b00781b
walsc024 <chriswalsh08@googlemail.com>
2021-03-14 13:47:41 +0000

    added routes folder

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useFindAndModify: false
 });
 
-app.use (require("./routes/api.js"));
+app.use (require("./public/api.js"));
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`)
