2bc7992e71d594288837f438c2b41f896fab28ab
walsc024 <chriswalsh08@googlemail.com>
2021-03-14 15:22:41 +0000

    html being served back

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -1,7 +1,14 @@
 const router = require("express").Router();
+const path = require("path")
 
 //route which will return exercises.html on /exercise
-
+router.get("/exercsie", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"))
+})
 //route that will return stats.html on /stats
+router.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/stats.html"))
+})
+
 
 module.exports = router; 
