01039a7a7afdf6dc0e06c1fbc93be975a4237500
walsc024 <chriswalsh08@googlemail.com>
2021-03-14 19:22:25 +0000

    added all and range routes

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,21 +1,56 @@
 const router = require("express").Router();
-const Workout = require("../models/workout")
+const Workout = require("../models/workout");
 
 //post route for adding a workout
 router.post("/api/workouts", (req, res) => {
-    Workout.create({}).then((workoutAdded) => {
-        res.json(workoutAdded)
-    }).catch((err) => {
-        res.json(err)
+  Workout.create({})
+    .then((workoutAdded) => {
+      res.json(workoutAdded);
     })
-})
+    .catch((err) => {
+      res.json(err);
+    });
+});
 //put route for updating a workout with an exercise
-
+router.put("/api/workouts/:id", (req, res) => {
+    Workout.findByIdAndUpdate(
+      req.params.id,
+      {
+        $push: { exercises: req.body },
+      },
+      { new: true, runValidators: true }
+    
+  )
+  .then((workoutAdded) => {
+    res.json(workoutAdded);
+  })
+  .catch((err) => {
+    res.json(err);
+  });
+});
 //get routes for getting all workouts
+
+router.get("/api/workouts", (req, res) => {
+    Workout.find({})
+    .then((dbWorkout) => {
+        res.json(dbWorkout);
+    })
+    .catch((err) => {
+        res.json(err);
+    });
+});
 
 //get route for getting the last 7 workouts
 
+router.get("/api/workouts/range", (req, res) => {
+    Workout.find ({})
+    .limit(7)
+    .then((dbWorkout) => {
+        res.json(dbWorkout);
+    })
+    .catch((err) => {
+        res.json(err)
+    })
+})
 
-
-
-module.exports = router; 
+module.exports = router;
